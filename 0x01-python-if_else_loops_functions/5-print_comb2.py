5c80e7b38dcfb5d6369a205840b5dad68c01e3f5
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-08-23 19:28:51 -0700

    0-99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=", ")
-    print(i + 1)
+for i in range(100):
+    if i < 99:
+        print("{:02d}".format(i), end=', ')
+    else:
+        print(i)
