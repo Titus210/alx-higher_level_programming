f16a0afe75d65821ae78ee1c4afc6eb6eed6f5f0
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-08-23 19:51:54 -0700

    comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        if i == 8:
-            print("{}{}".format(i, j))
-        else:
+for i in range(10):
+    for j in range(10):
+        if j > i and i != 8:
             print("{}{}".format(i, j), end=", ")
+        elif i == 8 and j == 9:
+            print("{}{}".format(i, j))
+            break
