dcff6f8ab822973e5fb9c6796a077bf26b9bf57a
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-08-29 17:32:12 -0700

    reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        new_list = my_list[::-1]
+        for i in range(len(new_list)):
+            print("{:d}".format(new_list[i]))
