7942f8975a05c0af92b4f995e11314285dda273f
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-09-02 18:54:01 -0700

    ipdate

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = 
-    else:
-        a_dictionary[key] = value
-    return a_dictionary
+    a_dictionary[key] = value
+    return (a_dictionary)
