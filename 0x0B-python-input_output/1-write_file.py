7e927b8f591259e2dd5a6ff9ef1308264031e778
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-09-28 16:28:58 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""Project 3-write_text.py
-Write text to a file
-"""
+"""Project 3-write_text."""
 
 
 def write_file(filename="", text=""):
-    """
-    Writting file to a fiile and creates one if it exists
-
-    Args:
-        - filename: name of file
-        - text: string to be written on file
-
-    Returns: characters written
-    """
-
-    with open("my_first_file.txt","w+",encoding = "uft-8") as f:
-        return f.write(text)
+    """"write file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
