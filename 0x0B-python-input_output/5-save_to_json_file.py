3128f0bdecd9fa55ff56dc3fbbc4663d59ca3766
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-09-28 16:34:08 -0700

    Save from Json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the "from_json_string" function
-"""
-
+"""save "from_json_string" function"""
 import json
 
 
-def from_json_string(my_str):
-    """returns an object represented by a JSON string"""
-    return json.loads(my_str)
+def save_to_json_file(my_obj, filename):
+    """save to json"""
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(json.dumps(my_obj))
