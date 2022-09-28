23cf65aa8d975dfb28096437a1f5d9cef22658a0
Titus Kiplagat <Tituskiplagat210@gmail.com>
2022-09-28 16:38:00 -0700

    load from json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the "save_to_json_file" function
-"""
-
+"""Load_to_json_file"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """writes an Object to a text file, using a JSON representation"""
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+def load_from_json_file(filename):
+    """load from json"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
